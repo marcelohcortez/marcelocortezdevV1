c07379740183618adfed02b0fd18a3736e097830
marcelohcortez <marcelohcortez@gmail.com>
2023-11-16 10:14:55 +0100

    Cleanup and start a new project

--- app/components/SingleDivider.tsx
+++
@@ -1,5 +0,0 @@
-export default function SingleDivider() {
-  return (
-    <span className="singleDivider flex bg-white h-0.5 w-[50%] m-auto"></span>
-  )
-}
--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/sobre',
-        destination: '/about',
-      },
-      {
-        source: '/contato',
-        destination: '/contact',
-      },
-    ]
-  },
-}
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
